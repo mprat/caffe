3a65b023aa5dfc380d6ce1a04a1666e7ff243107
Michele Pratusevich <mprat@mit.edu>
2014-12-05 14:04:18 -0500

    fixing the master status of my repo

--- sun_play/lmdb_python_test.py
+++
@@ -1,9 +0,0 @@
-import lmdb
-
-env = lmdb.open('sun_testing_01_lmdb_0_start/')
-
-with env.begin() as txn:
-    cursor = txn.cursor()
-    for key, value in cursor:
-        print ((key, value))
-        raw_input()
