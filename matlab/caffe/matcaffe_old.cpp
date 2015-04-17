d997a031b6e1267d7d5e4ce8b19684ec971d2018
Michele Pratusevich <mprat@mit.edu>
2015-04-16 23:38:52 -0400

    raw matcaffe.cpp with weird new bindings

--- matlab/caffe/matcaffe_old.cpp
+++ matlab/caffe/matcaffe_old.cpp
@@ -13,38 +13,6 @@
 
 #define MEX_ARGS int nlhs, mxArray **plhs, int nrhs, const mxArray **prhs
 
-#define CHECK_EQ_X(A, B, C, FUNC, ...) FUNC
-
-// The macro that the programmer uses
-#define CHECK_EQ_MEX(...)  CHECK_EQ_X(__VA_ARGS__,  \
-                       CHECK_EQ_3(__VA_ARGS__),    \
-                       CHECK_EQ_2(__VA_ARGS__))
-
-#define CHECK_EQ_2(a, b)  do {                                  \
-  if ( (a) != (b) ) {                                           \
-    fprintf(stderr, "%s:%d: Check failed because %s != %s\n",   \
-            __FILE__, __LINE__, #a, #b);                        \
-    mexErrMsgTxt("Error in CHECK");                             \
-  }                                                             \
-} while (0);
-
-#define CHECK_EQ_3(a, b, m)  do {                               \
-  if ( (a) != (b) ) {                                           \
-    fprintf(stderr, "%s:%d: Check failed because %s != %s\n",   \
-            __FILE__, __LINE__, #a, #b);                        \
-    fprintf(stderr, "%s:%d: %s\n",                              \
-            __FILE__, __LINE__, #m);                            \
-    mexErrMsgTxt(#m);                                           \
-  }                                                             \
-} while (0);
-
-#define CUDA_CHECK_MEX(condition) \
-  /* Code block avoids redefinition of cudaError_t error */ \
-  do { \
-    cudaError_t error = condition; \
-    CHECK_EQ_MEX(error, cudaSuccess, "CUDA_CHECK")  \
-  } while (0)
-  
 // Log and throw a Mex error
 inline void mex_error(const std::string &msg) {
   LOG(ERROR) << msg;
@@ -56,12 +24,6 @@ using namespace caffe;  // NOLINT(build/namespaces)
 // The pointer to the internal caffe::Net instance
 static shared_ptr<Net<float> > net_;
 static int init_key = -2;
-
-static void CheckFile(const string& filename) {
-    std::ifstream f(filename.c_str());
-    CHECK_EQ_MEX(f.good(), true, "Could not open file " + filename);
-    f.close();
-}
 
 // Five things to be aware of:
 //   caffe uses row-major order
@@ -402,174 +364,6 @@ static void read_mean(MEX_ARGS) {
     plhs[0] = mx_blob;
 }
 
-static int do_get_num_layers() {
-  return net_->blobs().size();
-}
-
-static mxArray* do_get_layer(int L) {
-  const shared_ptr< Blob<float> > layer_blob = net_->blobs()[L];
-  mwSize dims[4] = {layer_blob->width(), layer_blob->height(), layer_blob->channels(), layer_blob->num()};
-  mxArray* mx_layer = mxCreateNumericArray(4, dims, mxSINGLE_CLASS, mxREAL);
-  float* layer_ptr = reinterpret_cast<float*>(mxGetPr(mx_layer));
-  
-  switch (Caffe::mode()) {
-    case Caffe::CPU:
-      memcpy(layer_ptr, layer_blob->cpu_data(),
-          sizeof(float) * layer_blob->count());
-      break;
-    case Caffe::GPU:
-      CUDA_CHECK(cudaMemcpy(layer_ptr, layer_blob->gpu_data(),
-          sizeof(float) * layer_blob->count(), cudaMemcpyDeviceToHost));
-      break;
-    default:
-      LOG(FATAL) << "Unknown caffe mode: " << Caffe::mode();
-  }
-  return mx_layer;
-}
-
-static mxArray* do_get_all_layers() {
-  int n = do_get_num_layers();
-  const mwSize dims[2] = {n, 1};
-  mxArray* mx_layer_cells = mxCreateCellArray(2, dims);
-  
-  for(int i=0; i<n; ++i){
-    mxSetCell(mx_layer_cells, i, do_get_layer(i));
-  }
-  
-  return mx_layer_cells;
-}
-
-static void get_num_layers(MEX_ARGS) {
-  plhs[0] = mxCreateDoubleScalar(do_get_num_layers());
-}
-
-static mxArray* do_get_layer_name(int L) {
-  return mxCreateString(net_->blob_names()[L].c_str());
-}
-
-static mxArray* do_get_all_layer_names() {
-  int n = do_get_num_layers();
-  const mwSize dims[2] = {n, 1};
-  mxArray* mx_layer_names = mxCreateCellArray(2, dims);
-  
-  for(int i=0; i<n; ++i){
-    mxSetCell(mx_layer_names, i, do_get_layer_name(i));
-  }
-  
-  return mx_layer_names;
-}
-
-static void get_layer(MEX_ARGS) {
-  int L = static_cast<int>(mxGetScalar(prhs[0]));
-  plhs[0] = do_get_layer(L);
-  plhs[1] = do_get_layer_name(L);
-}
-
-static void get_all_layers(MEX_ARGS) {
-  plhs[0] = do_get_all_layers();
-  plhs[1] = do_get_all_layer_names();
-}
-
-static void get_all_layer_names(MEX_ARGS) {
-  plhs[0] = do_get_all_layer_names();
-}
-
-static void get_layer_name(MEX_ARGS){
-  int L = static_cast<int>(mxGetScalar(prhs[0]));
-  plhs[0] = do_get_layer_name(L);
-}
-
-static void mxarray_to_blob_data(const mxArray* data, Blob<float>* blob) {
-  mwSize dims[4] = {blob->width(), blob->height(),
-                    blob->channels(), blob->num()};
-  LOG(INFO) << dims[0] << " " << dims[1] << " " << dims[2] << " " << dims[3];
-  CHECK_EQ_MEX(blob->count(), mxGetNumberOfElements(data),
-    "blob->count() don't match with numel(data)");
-  const float* const data_ptr =
-      reinterpret_cast<const float* const>(mxGetPr(data));
-  switch (Caffe::mode()) {
-  case Caffe::CPU:
-    caffe_copy(blob->count(), data_ptr, blob->mutable_cpu_data());
-    break;
-  case Caffe::GPU:
-    caffe_copy(blob->count(), data_ptr, blob->mutable_gpu_data());
-    break;
-  default:
-    LOG(FATAL) << "Unknown Caffe mode.";
-    mexErrMsgTxt("Unknown caffe mode");
-  }  // switch (Caffe::mode())
-}
-
-static void mxarray_to_blob_data(const mxArray* data,
-    shared_ptr<Blob<float> > blob) {
-  mxarray_to_blob_data(data, blob.get());
-}
-
-static void save_net(MEX_ARGS) {
-  if (nrhs != 1) {
-    LOG(ERROR) << "Only given " << nrhs << " arguments";
-    mexErrMsgTxt("Wrong number of arguments");
-  }
-  if (net_) {
-  char* model_file = mxArrayToString(prhs[0]);
-  NetParameter net_param;
-  net_->ToProto(&net_param, false);
-  WriteProtoToBinaryFile(net_param, model_file);
-  CheckFile(string(model_file));
-  mxFree(model_file);
-  } else {
-    mexErrMsgTxt("Need to have a network to save");
-  }
-}
-
-static void do_set_layer_weights(const mxArray* const layer_name,
-    const mxArray* const mx_layer_weights) {
-  const vector<shared_ptr<Layer<float> > >& layers = net_->layers();
-  const vector<string>& layer_names = net_->layer_names();
-
-  char* c_layer_name = mxArrayToString(layer_name);
-  LOG(INFO) << "Looking for: " << c_layer_name;
-
-  for (unsigned int i = 0; i < layers.size(); ++i) {
-    LOG(INFO) << layer_names[i];
-    if (strcmp(layer_names[i].c_str(), c_layer_name) == 0) {
-      vector<shared_ptr<Blob<float> > >& layer_blobs = layers[i]->blobs();
-      if (layer_blobs.size() == 0) {
-        continue;
-      }
-      LOG(INFO) << "Found layer " << layer_names[i];
-      CHECK_EQ_MEX(static_cast<unsigned int>(
-        mxGetDimensions(mx_layer_weights)[0]),
-        layer_blobs.size(), "Num of cells don't match layer_blobs.size");
-      LOG(INFO) << "layer_blobs.size() = " << layer_blobs.size();
-      for (unsigned int j = 0; j < layer_blobs.size(); ++j) {
-        // internally data is stored as (width, height, channels, num)
-        // where width is the fastest dimension
-        const mxArray* const elem = mxGetCell(mx_layer_weights, j);
-        mxarray_to_blob_data(elem, layer_blobs[j]);
-      }
-    }
-  }
-}
-
-static void set_weights(MEX_ARGS) {
-  if (nrhs != 1) {
-    LOG(ERROR) << "Given " << nrhs << " arguments expecting 1";
-    mexErrMsgTxt("Wrong number of arguments");
-  }
-  const mxArray* const mx_weights = prhs[0];
-  if (!mxIsStruct(mx_weights)) {
-    mexErrMsgTxt("Input needs to be struct");
-  }
-  int num_layers = mxGetNumberOfElements(mx_weights);
-  for (int i = 0; i < num_layers; ++i) {
-    const mxArray* layer_name= mxGetField(mx_weights, i, "layer_names");
-    const mxArray* weights= mxGetField(mx_weights, i, "weights");
-    do_set_layer_weights(layer_name, weights);
-  }
-}
-
-
 /** -----------------------------------------------------------------
  ** Available commands.
  **/
@@ -589,15 +383,8 @@ static handler_registry handlers[] = {
   { "set_device",         set_device      },
   { "get_weights",        get_weights     },
   { "get_init_key",       get_init_key    },
-  { "get_layer",          get_layer       },
-  { "get_all_layers",     get_all_layers  },
-  { "get_num_layers",     get_num_layers  },
   { "reset",              reset           },
-  { "get_names",          get_all_layer_names},
-  { "get_name",           get_layer_name  },
   { "read_mean",          read_mean       },
-  { "save_net",           save_net        },
-  { "set_weights",        set_weights     },
   // The end.
   { "END",                NULL            },
 };
@@ -632,4 +419,3 @@ void mexFunction(MEX_ARGS) {
     mxFree(cmd);
   }
 }
-
